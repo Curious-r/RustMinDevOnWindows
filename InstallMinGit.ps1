9d75ff267535e9148ee0f99bdf92a8a84fa112d8
Curious <13582788510@139.com>
2022-06-20 16:52:06 +0800

    Add files via upload

--- InstallMinGit.ps1
+++ InstallMinGit.ps1
@@ -21,7 +21,7 @@ function Add-Path-Crs {
     [CmdletBinding()]
     param (
         # 声明可以接受管道输入的参数 $InputObject ，PowerShell原生cmdlet中承接管道输入的参数都叫这个名字，因此，
-        # 在自定义函数中尽量也要继承这个写法，以增加代码的可读性
+        # 在自定义函数中尽量也要继承这个写法，以增加代码的可读性。
         [Parameter(Mandatory = $true, ParameterSetName = 'AddPath', ValueFromPipeline = $true, Position = 0)]
         [String[]]$InputObject,
         [Parameter(ParameterSetName = 'AddPath')]
@@ -35,13 +35,13 @@ function Add-Path-Crs {
 
         $InputCombiner = New-Object -TypeName System.Text.StringBuilder
 
-        # 将要添加的值拼接到一个可变字符串中
+        # 将要添加的值拼接到一个可变字符串中：
         $InputObject | ForEach-Object {
             [Void]$InputCombiner.Append(';')
             [Void]$InputCombiner.Append($_.Trim())
         }
 
-        # 获取环境变量Path
+        # 获取环境变量Path：
         $Path = (Get-Item -Path $EnvironmentRegisterKey).GetValue(
             "PATH",
             "",
@@ -50,7 +50,7 @@ function Add-Path-Crs {
             $Path = $Path.Remove($Path.length - 1, 1)
         }
 
-        # 更新环境变量Path
+        # 更新环境变量Path：
         $Path = $Path + $InputCombiner.ToString()
         Set-ItemProperty -Path $EnvironmentRegisterKey -Name Path -Value $Path
     }
@@ -71,7 +71,7 @@ function Remove-Path-Crs {
             $EnvironmentRegisterKey = 'HKLM:\SYSTEM\ControlSet001\Control\Session Manager\Environment'
         } 
 
-        # 遍历 $InputObject ，删除环境变量Path中的对应的路径
+        # 遍历 $InputObject ，删除环境变量Path中的对应的路径：
         $InputObject | ForEach-Object {
             $Path = (Get-Item -Path $EnvironmentRegisterKey).GetValue(
                 "PATH",
@@ -79,7 +79,7 @@ function Remove-Path-Crs {
                 [Microsoft.Win32.RegistryValueOptions]::DoNotExpandEnvironmentNames)
 
             # 在参数开头添加分号，末尾添加反斜杠，以便后续先进行条件最严格的匹配，再逐步退让匹配条件，
-            # 以免在Path中留下残余的无用符号
+            # 以免在Path中留下残余的无用符号。
             $EntryToRemove = ';' + $_.Trim()
             if (!$EntryToRemove.EndsWith('\')) {
                 $EntryToRemove += '\'
@@ -88,26 +88,26 @@ function Remove-Path-Crs {
             $EntryToRemove = [Regex]::Escape($EntryToRemove)
 
             # 检测 $EntryToRemove 在 $Path 中是否有匹配，如果 $EntryToRemove 在 $Path 中没有匹配，
-            # 存在 $Path 中相应条目不以反斜杠结尾的可能性，尝试去掉 $EntryToRemove 末尾的反斜杠
+            # 存在 $Path 中相应条目不以反斜杠结尾的可能性，尝试去掉 $EntryToRemove 末尾的反斜杠。
             if (!($Path -match $EntryToRemove)) {
                 $EntryToRemove = $EntryToRemove.SubString(0, $EntryToRemove.LastIndexOf('\') - 1)
             }
 
             # 检测 $EntryToRemove 在 $Path 中是否有匹配，若存在对应值，则执行删除操作，并提前返回，
-            # ForEach-Object 将处理管道传入的集合中的下一个对象
+            # ForEach-Object 将处理管道传入的集合中的下一个对象。
             if ($Path -match $EntryToRemove) {
                 $Path = $Path -replace $EntryToRemove, ""
                 Set-ItemProperty -Path $EnvironmentRegisterKey -Name Path -Value $Path
                 return
             }
 
-            # 如果上述判断没有匹配，考虑到Path的结构形式，那么除了确实没有相应条目以外，还存在相应条目在Path
-            # 第一条，以至于前边没有分号的可能性。接下来尝试使用不加分号的值进行匹配，流程与前述类似
+            # 如果上述判断没有匹配，考虑到Path的结构形式，那么除了确实没有相应条目以外，还存在相应条目在
+            # Path第一条，以至于前边没有分号的可能性。接下来尝试使用不加分号的值进行匹配，流程与前述类似。
             $EntryToRemove = $_.Trim()
             if (!$EntryToRemove.EndsWith('\')) {
                 $EntryToRemove += '\'
             }
-            # 转义反斜杠以适应正则表达式
+            # 转义反斜杠以适应正则表达式：
             $EntryToRemove = [Regex]::Escape($EntryToRemove)
             if (!($Path -match $EntryToRemove)) {
                 $EntryToRemove = $EntryToRemove.Substring(0, $EntryToRemove.LastIndexOf('\') - 1)
@@ -117,21 +117,21 @@ function Remove-Path-Crs {
                 Set-ItemProperty -Path $EnvironmentRegisterKey -Name Path -Value $Path
             }
 
-            # 如果仍然没有匹配，那么说明确实没有对应条目，流程运行完毕自动退出
+            # 如果仍然没有匹配，那么说明确实没有对应条目，流程运行完毕自动退出。
         }
     }
 }
 
 
-# 设置Git安装位置，以下两行为默认安装在用户安装目录，如需改动请一并修改
+# 设置Git安装位置，以下两行为默认安装在用户安装目录，如需改动请一并修改。
 $GitInstallationLocation = "$env:LOCALAPPDATA\Programs\Git"
-# 用户环境变量Path中Git的内容
+# 用户环境变量Path中Git的内容：
 $GitPath = ($GitInstallationLocation -replace [Regex]::Escape($env:LOCALAPPDATA), "%LOCALAPPDATA%") `
     + "\cmd"
 
-# 安装Git
+# 安装Git的函数封装：
 function Install-Git-Crs {
-    # 从GitHub下载最新发布的BusyBox版MinGit
+    # 从GitHub下载最新发布的BusyBox版MinGit：
     $LatestReleaseContent = Invoke-WebRequest https://github.com/git-for-windows/git/releases/latest |
     Select-Object -ExpandProperty Content
     $LatestReleaseContent -match `
@@ -140,19 +140,19 @@ function Install-Git-Crs {
     Invoke-WebRequest ("https://github.com" + $Matches[0]) -OutFile $PSScriptRoot\Git.zip
     Write-Host "Download complete."
 
-    # 安装前清理Git安装路径
+    # 安装前清理Git安装路径：
     if ((Test-Path $GitInstallationLocation) -eq 1) {
         Remove-Item -Recurse -Force $GitInstallationLocation
     } 
 
-    # 解压到目标路径
+    # 解压到目标路径：
     Write-Host "Decompressing..."
     Expand-Archive -LiteralPath $PSScriptRoot\Git.zip -DestinationPath $GitInstallationLocation
     Remove-Item $PSScriptRoot\Git.zip
     Write-Host "Complete."
 }
 
-# 移除由本脚本安装的Git
+# 移除由本脚本安装的Git的函数封装：
 function Uninstall-Git-Crs {
     if ((Test-Path $GitInstallationLocation) -eq 1) {
         Remove-Item -Recurse -Force $GitInstallationLocation
@@ -167,7 +167,7 @@ switch ($args) {
         # 添加到用户环境变量,支持管道写法，取消注释下方语句：
         #$GitPath | Add-Path-Crs
         Add-Path-Crs $GitPath
-        # 启用Windows的长路径支持
+        # 启用Windows的长路径支持：
         Write-Host "Enbaling long path supoort in Windows..."
         New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem" `
             -Name "LongPathsEnabled" -Value 1 -PropertyType DWORD -Force
