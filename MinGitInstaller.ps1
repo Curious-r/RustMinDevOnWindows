f80d5b7a770dc7fca823e36b02b7ae338cd98bce
Curious <13582788510@139.com>
2022-12-23 23:28:46 +0800

    Add files via upload

--- MinGitInstaller.ps1
+++ MinGitInstaller.ps1
@@ -10,15 +10,14 @@ $GitPath = ($GitInstallationLocation -replace [Regex]::Escape($env:LOCALAPPDATA)
     + "\cmd"
 
 # 安装Git的函数封装：
-function Install-Git-Crs {
+function Expand-Git-Crs {
     # 从GitHub下载最新发布的BusyBox版MinGit：
-    $LatestReleaseContent = Invoke-WebRequest https://github.com/git-for-windows/git/releases/latest |
+    $LatestReleaseContent = Invoke-WebRequest https://github.com/git-for-windows/git/releases |
     Select-Object -ExpandProperty Content
     $LatestReleaseContent -match `
         "/git-for-windows/git/releases/download/v.*.windows.1/MinGit-.*-busybox-64-bit.zip"
-    Write-Host "Download start."
+    Write-Host "Download MinGit-busybox-64bit.zip."
     Invoke-WebRequest ("https://github.com" + $Matches[0]) -OutFile $PSScriptRoot\Git.zip
-    Write-Host "Download complete."
 
     # 安装前清理Git安装路径：
     if ((Test-Path $GitInstallationLocation) -eq 1) {
@@ -33,7 +32,7 @@ function Install-Git-Crs {
 }
 
 # 移除由本脚本安装的Git的函数封装：
-function Uninstall-Git-Crs {
+function Remove-Git-Crs {
     if ((Test-Path $GitInstallationLocation) -eq 1) {
         Remove-Item -Recurse -Force $GitInstallationLocation
     }
@@ -43,26 +42,26 @@ function Uninstall-Git-Crs {
 # 脚本运行入口
 switch ($args) {
     "install" { 
-        Install-Git-Crs
-        # 添加到用户环境变量,支持管道写法，取消注释下方语句：
+        Expand-Git-Crs
+        # 添加到用户环境变量,支持管道写法
         #$GitPath | Add-Path-Crs
         Add-Path-Crs $GitPath
         # 启用Windows的长路径支持：
-        Write-Host "Enbaling long path supoort in Windows..."
+        Write-Host "Enbaling long path supoort in Windows, this will take effect after reboot."
         New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem" `
             -Name "LongPathsEnabled" -Value 1 -PropertyType DWORD -Force
-        Write-Host "Please reboot your machine to make it take effect."
+
         Write-Host "Install complete."
         Return 
     }
     "update" { 
-        Install-Git-Crs
+        Expand-Git-Crs
         Write-Host "Update complete."
         Return 
     }
     "uninstall" { 
-        Uninstall-Git-Crs
-        # 删除注册的环境变量,支持管道写法，取消注释下方语句：
+        Remove-Git-Crs
+        # 删除注册的环境变量,支持管道写法
         #$GitPath | Remove-Path-Crs
         Remove-Path-Crs $GitPath
         Write-Host "Uninstall complete."
